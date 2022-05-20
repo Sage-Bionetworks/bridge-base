1107864058050b6457992f04392d5039a96f3f9e
Alx Dark <alx.dark@sagebase.org>
2022-05-20 10:29:30 -0700

    removing unused import

--- src/test/java/org/sagebionetworks/bridge/config/PropertiesConfigTest.java
+++ src/test/java/org/sagebionetworks/bridge/config/PropertiesConfigTest.java
@@ -5,7 +5,6 @@ import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertNull;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
